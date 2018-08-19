08f5cb46901d817a7cc86ffc14fc0874a25ca979
nicolasmaurotorres <torres.nicolas.mauro@gmail.com>
2018-08-19 00:41:31 -0300

    visual correction

--- src/js/components/common/Modal.js
+++ src/js/components/common/Modal.js
@@ -35,7 +35,7 @@ class Modal extends React.Component {
               <div className="text">{ text }</div>
               <div className="buttons">
                   <button className="modal-button" onClick={() => this.onConfirm()}>Confirm</button>
-                  <button className="modal-button" onClick={() => this.onClose()}>Close</button>
+                  <button className="close-notification" onClick={() => this.onClose()}>Close</button>
               </div>
               </div>
           </div>
@@ -47,7 +47,7 @@ class Modal extends React.Component {
             <div className="modal-wrapper" style={{zIndex: (zIndex+1)*10}}>
               <div className="modal">
               { React.cloneElement(content,{ callback : this.onClose }) }
-                <button className="close" onClick={() => this.onClose()}>&times;</button>
+                <button className="close-notification" onClick={() => this.onClose()}>&times;</button>
               </div>
             </div>
           );
