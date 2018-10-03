2d6f28d670d0accd7d4034dd73576795c40675a5
nicolasmaurotorres <torres.nicolas.mauro@gmail.com>
2018-10-02 22:46:22 -0300

    moved the render config to another file

---
+++ src/js/config/renderConfig.js
@@ -0,0 +1,11 @@
+const renderConfig = {
+    sessionManagerURL : 'http://localhost:8080/paraview', // fix
+    node: 1024, // i dont know what is this
+    application: 'visualizer', // fix - visualizer
+    secret: 'katglrt54#%dfg', // it could bepodria no estar pero bue...
+    user: 'sebastien.jourdain', // podria no estar pero bue...
+    password: 'ousdfbdxldfgh', // podria no estar pero bue...
+    //sessionURL : "ws://localhost:8080/ws",
+    //sessionURL : "http://localhost:8080/paraview" // PRUEBA 
+    //app: 'Visualizer'
+  }
