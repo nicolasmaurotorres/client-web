9235d25273cd1b4c001c9c45115f4d98eff8b693
Mariano Torres <torres.nicolas.mauro@gmail.com>
2019-02-03 17:47:04 -0300

    deletes unnused files

--- src/js/components/NotImplementedYet.js
+++
@@ -1,9 +0,0 @@
-import React    from 'react'
-
-export default class NotImplementedYet extends React.Component {
-    render(){
-        return (
-            <div>Not Implemented Yet</div>
-        );
-    }
-}
--- src/js/utils/confirmDialog.js
+++
@@ -1,5 +0,0 @@
-import { createConfirmation } from 'react-confirm';
- 
-export default function(modal,title,message = {} ) {
-  return createConfirmation(modal)({title,message});
-}
