60425ff6a539a0e307e729cbed176b08931d51f4
Mariano Torres <torres.nicolas.mauro@gmail.com>
2019-02-19 16:52:32 -0300

    fix file path 2

--- src/js/renders/VisualizerServerRender.js
+++ src/js/renders/VisualizerServerRender.js
@@ -15,7 +15,7 @@ class VisualizerServerRender extends React.Component {
         var config = renderConfig;
         config['dataDir'] = config['dataDir']+ dataDir;
         console.log('dataDir: '+config['dataDir']);
-        config['dataFile'] = config['dataDir'] + dataFile;
+        config['dataFile'] =  dataFile;
         console.log('dataFile'+ config['dataFile'] );
         Visualizer.connectWithArgsAsConfig(renderConfig);
         Visualizer.autoStopServer(10);
