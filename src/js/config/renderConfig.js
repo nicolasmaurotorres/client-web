518329957b32b7bd887070fe04c0f22c18418605
Mauro <torres.nicolas.mauro@gmail.com>
2019-08-05 00:55:55 -0300

    client dockerized

--- src/js/config/renderConfig.js
+++ src/js/config/renderConfig.js
@@ -1,10 +1,10 @@
 export const renderConfig = {
-    sessionManagerURL : 'http://localhost:8080/paraview', // fix data
+    sessionManagerURL : 'http://localhost:8082/paraview', // fix data
     node: 1024, // i dont know what is this
     application: 'visualizer', // fix data - visualizer
     secret: 'katglrt54#%dfg', // it could not be this but...
     user: 'sebastien.jourdain', // it could not be this but...
     password: 'ousdfbdxldfgh', // it could not be this but...
-    dataDir : '/data',
+    dataDir : '/opt/wslink-launcher/shared',
     dataFile : '' // filled dinamically
 }
